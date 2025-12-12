b868abf4b6223278a837b6d34671ab5bae99e992
CHANDAN10008 <139030891+CHANDAN10008@users.noreply.github.com>
2025-12-12 12:22:41 +0530

    Update Contact map to Kolkata

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
                         {/* Map Embed (Placeholder) */}
                         <div className="w-full h-80 bg-white/5 rounded-3xl overflow-hidden border border-white/10 relative">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.98555098464!2d-122.507640204439!3d37.757814996609724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan%20Francisco%2C%20CA!5e0!3m2!1sen!2sus!4v1645564756877!5m2!1sen!2sus"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117925.2168969446!2d88.26495057896431!3d22.5354063744826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1645564756877!5m2!1sen!2sin"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
